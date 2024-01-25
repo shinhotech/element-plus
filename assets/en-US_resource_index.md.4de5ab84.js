02124466d071d459b20d932da97cda60b8be433c
21060033 <chengshuli@shinho.net.cn>
2024-01-25 17:12:06 +0800

    文档测试

--- assets/en-US_resource_index.md.4de5ab84.js
+++
@@ -1,1 +0,0 @@
-import{d as t,a,r as o,o as r}from"./app.941a47d6.js";const p='{"title":"","description":"","frontmatter":{"page":true,"lang":"en-US"},"headers":[],"relativePath":"en-US/resource/index.md","lastUpdated":1645671896000}',n={},d=Object.assign(n,{setup(s){return(c,_)=>{const e=o("Resource");return r(),t("div",null,[a(e)])}}});export{p as __pageData,d as default};
